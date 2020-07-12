c01ae8729aaa21e1b7e04b2957460b498e7e2553
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-12 16:10:41 -0700

    partial implementation of median Maintenance

--- chapter_11/src/MedianMaintenance.java
+++ chapter_11/src/MedianMaintenance.java
@@ -1,13 +1,15 @@
+import edu.princeton.cs.algs4.*;
 import edu.princeton.cs.algs4.AVLTreeST;
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Queue;
 
 public class MedianMaintenance {
     public static void main(String[] args) {
         In in = new In(args[0]);
         Queue<Integer> result = new Queue<>();
-        MinPQ<Integer> minpq = new MinPQ<>(10);
-        MaxPQ<Integer> maxpq = new MaxPQ<>(10);
+        MinPQ<Integer> minpq = new MinPQ<>(10001);
+        MaxPQ<Integer> maxpq = new MaxPQ<>(10001);
+
+
+        Stopwatch timer1 = new Stopwatch();
         while(!in.isEmpty()){
             minpq.insert(in.readInt());
             maxpq.insert(minpq.delMax());
@@ -18,6 +20,8 @@ public class MedianMaintenance {
             int value = (maxpq.size() == minpq.size()) ? maxpq.peek() : minpq.peek();
             result.enqueue(value);
         }
+        double time1 = timer1.elapsedTime();
+        StdOut.printf("(%.2f seconds)\n", time1);
 
         int running_sum = 0;
         for(int e : result){
@@ -29,6 +33,8 @@ public class MedianMaintenance {
 
         AVLTreeST<Integer, Integer> avl = new AVLTreeST<>();
         Queue<Integer> values = new Queue<>();
+
+        Stopwatch timer2 = new Stopwatch();
         while(!in.isEmpty()){
             avl.put(in.readInt(), 1);
             int k = avl.size() - 1;
@@ -40,6 +46,9 @@ public class MedianMaintenance {
                 values.enqueue(avl.select(select));
             }
         }
+        double time2 = timer2.elapsedTime();
+        StdOut.printf("(%.2f seconds)\n", time2);
+
 
         int running_sum2 = 0;
         for(int e : values){
