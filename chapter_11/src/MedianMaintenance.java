985f2f87ba67142fbc355839eb8de1c8a471e5e4
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-11 17:10:40 -0700

    partial implementation

--- chapter_11/src/MedianMaintenance.java
+++ chapter_11/src/MedianMaintenance.java
@@ -1,17 +1,27 @@
+import edu.princeton.cs.algs4.AVLTreeST;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Queue;
 
 public class MedianMaintenance {
     public static void main(String[] args) {
         In in = new In(args[0]);
-        MaxPQ<Integer> maxpq = new MaxPQ<>(10);
-        MinPQ<Integer> minpq = new MinPQ<>(10);
+        AVLTreeST<Integer, Integer> avltree = new AVLTreeST<>();
         Queue<Double> result = new Queue<>();
-
+        int k = 0;
         while(!in.isEmpty()){
-
+            avltree.put(in.readInt(), k);
+            if(avltree.size() == 1){
+                result.enqueue((double)avltree.min());
+            }
+            else if(avltree.size() % 2 == 1){
+                result.enqueue((double)avltree.select(k));
+            } else {
+                int kth = avltree.select(k);
+                int kth_plus_one = avltree.select(k - 1);
+                result.enqueue((kth + kth_plus_one) / 2.0);
+            }
+            k++;
         }
-
         System.out.println(result);
     }
 }
