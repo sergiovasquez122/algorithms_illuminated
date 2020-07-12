8b9c9bb0f5f21ffca9e8faf387f1e4469d7a9344
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-12 16:00:08 -0700

    added avltree implementation of median maintenance

--- chapter_11/src/MedianMaintenance.java
+++ chapter_11/src/MedianMaintenance.java
@@ -5,7 +5,7 @@ import edu.princeton.cs.algs4.Queue;
 public class MedianMaintenance {
     public static void main(String[] args) {
         In in = new In(args[0]);
-        Queue<Double> result = new Queue<>();
+        Queue<Integer> result = new Queue<>();
         MinPQ<Integer> minpq = new MinPQ<>(10);
         MaxPQ<Integer> maxpq = new MaxPQ<>(10);
         while(!in.isEmpty()){
@@ -15,15 +15,37 @@ public class MedianMaintenance {
                 minpq.insert(maxpq.delMax());
             }
 
-            double value = (maxpq.size() == minpq.size()) ? maxpq.peek() : minpq.peek();
+            int value = (maxpq.size() == minpq.size()) ? maxpq.peek() : minpq.peek();
             result.enqueue(value);
         }
 
-        double running_sum = 0;
-        for(double e : result){
+        int running_sum = 0;
+        for(int e : result){
             running_sum += e;
         }
         System.out.println(running_sum);
 
+        in = new In(args[0]);
+
+        AVLTreeST<Integer, Integer> avl = new AVLTreeST<>();
+        Queue<Integer> values = new Queue<>();
+        while(!in.isEmpty()){
+            avl.put(in.readInt(), 1);
+            int k = avl.size() - 1;
+            if(k % 2 == 0){
+                int select = (k + 1) / 2;
+                values.enqueue(avl.select(select));
+            } else {
+                int select = k / 2;
+                values.enqueue(avl.select(select));
+            }
+        }
+
+        int running_sum2 = 0;
+        for(int e : values){
+            running_sum2 += e;
+        }
+        System.out.println(running_sum2);
     }
 }
+
