60a5ad559db1daed947e0c700e68fad8108b34fa
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-31 12:41:41 -0700

    implementation of Digraph

--- chapter_10/EdgeWeightedDigraph.java
+++ chapter_10/EdgeWeightedDigraph.java
@@ -1,2 +1,32 @@
-public class EdgeWeightedDigrap {
+import edu.princeton.cs.algs4.Bag;
+
+public class EdgeWeightedDigraph {
+    private final int V;
+    private int E;
+    private Bag<DirectedEdge>[] adj;
+
+    public EdgeWeightedDigraph(int V){
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<DirectedEdge>[]) new Bag[V];
+        for(int i = 0;i < V;i++)
+            adj[i] = new  Bag<>();
+    }
+
+    public void addEdge(DirectedEdge e){
+        adj[e.from()].add(e);
+        E++;
+    }
+
+    public Iterable<DirectedEdge> adj(int v){
+        return adj[v];
+    }
+
+    public int V(){
+        return V;
+    }
+
+    public int E(){
+        return E;
+    }
 }
