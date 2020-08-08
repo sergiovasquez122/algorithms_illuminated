f7d439725f9d4191c3528013520da61a9ec02a95
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-08 16:25:23 -0700

    added partition method

--- chapter__5/src/QuickSort.java
+++ chapter__5/src/QuickSort.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class QuickSort {
+
+    private void swap(int A[], int i, int j){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+
+    private int partition(int A[], int l, int r){
+        int p = A[l];
+        int i = l + 1;
+        for(int j = l + 1;j <= r; ++j){
+            if(A[j] < p){
+                swap(A, i, j);
+                i++;
+            }
+        }
+        swap(A, l, i - 1);
+        return i - 1;
+    }
 }
