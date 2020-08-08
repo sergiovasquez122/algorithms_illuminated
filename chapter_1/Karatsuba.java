adec5d4734b0df85b83271d037c8f630d884820c
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-08 14:59:57 -0700

    started implementation

--- chapter_1/Karatsuba.java
+++ chapter_1/Karatsuba.java
@@ -1,2 +1,10 @@
+import java.math.BigInteger;
+
 public class Karatsuba {
+
+    public static BigInteger karatsuba(BigInteger x, BigInteger y){
+        int N = Math.min(x.bitLength(), y.bitLength());
+        if(N == 1) return x.multiply(y);
+        return null;
+    }
 }
