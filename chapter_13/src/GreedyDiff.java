cc62ec149a96b07261adccaa0fb4dffdb901da66
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-12 15:12:29 -0700

    added more questions

--- chapter_13/src/GreedyDiff.java
+++ chapter_13/src/GreedyDiff.java
@@ -1,2 +1,16 @@
 public class GreedyDiff {
+
+    private class Node implements Comparable<Node> {
+        private int weight;
+        private int length;
+        public Node(int weight, int length){
+            this.weight = weight;
+            this.length = length;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return 0;
+        }
+    }
 }
