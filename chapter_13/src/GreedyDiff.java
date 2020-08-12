86be90a3dbef6be5ec5d439fac649f6ec1e5404a
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-12 16:30:39 -0700

    implementation of GreedyDiff using priority queue

--- chapter_13/src/GreedyDiff.java
+++ chapter_13/src/GreedyDiff.java
@@ -1,6 +1,10 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MaxPQ;
+import edu.princeton.cs.algs4.StdOut;
+
 public class GreedyDiff {
 
-    private class Node implements Comparable<Node> {
+    private static class Node implements Comparable<Node> {
         private int weight;
         private int length;
         public Node(int weight, int length){
@@ -10,7 +14,32 @@ public class GreedyDiff {
 
         @Override
         public int compareTo(Node o) {
-            return 0;
+            int thisCost = weight - length;
+            int theirCost = o.weight - o.length;
+            if(thisCost < theirCost) return -1;
+            else if(thisCost > theirCost) return 1;
+            else {
+                return Integer.compare(weight, o.weight);
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        MaxPQ<Node> maxPQ = new MaxPQ<>();
+        in.readLine();
+        while(in.hasNextLine()){
+            int weight = in.readInt();
+            int height = in.readInt();
+            maxPQ.insert(new Node(weight, height));
+        }
+        int completion_time = 0;
+        int running_sum = 0;
+        while(!maxPQ.isEmpty()){
+            Node x = maxPQ.delMax();
+            running_sum += x.length;
+            completion_time += running_sum * x.weight;
+        }
+        StdOut.println(completion_time);
     }
 }
