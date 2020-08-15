ef909aed0bc56fc44ca7fe202aaf94b8de206231
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-14 19:06:33 -0700

    added Node class

--- chapter_14/src/Huffman.java
+++ chapter_14/src/Huffman.java
@@ -1,2 +1,26 @@
 public class Huffman {
+
+    private static class Node implements Comparable<Node>
+    {
+        private int value;
+        private int freq;
+        private final Node left, right;
+
+        public Node(int value, int freq, Node left, Node right) {
+            this.value = value;
+            this.freq = freq;
+            this.left = left;
+            this.right = right;
+        }
+
+        public boolean isLeaf(){
+            return left == null && right == null;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(freq, o.freq);
+        }
+    }
+
 }
