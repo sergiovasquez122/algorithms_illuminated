718a9a00a2c4b980be61d56b467196aecf27ca53
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-19 14:44:36 -0700

    added WIS

---
+++ chapter_16/src/WIS.java
@@ -0,0 +1,18 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class WIS {
+    public static int compute(int pathGraph[]){
+        int[] A = new int[pathGraph.length + 1];
+        A[0] = 0;
+        A[1] = pathGraph[0];
+        for(int i = 2;i <= pathGraph.length;i++){
+            A[i] = Math.max(A[i - 1], A[i - 2] + pathGraph[i - 1]);
+        }
+        return A[pathGraph.length];
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1, 4, 5, 4};
+        StdOut.println(compute(A));
+    }
+}
