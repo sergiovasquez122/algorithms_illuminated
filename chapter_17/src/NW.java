c6566509714c1745c10a6af68c434c9c7832f8eb
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-21 16:34:09 -0700

    added datatset1.txt

---
+++ chapter_17/src/NW.java
@@ -0,0 +1,26 @@
+public class NW {
+    public static int NWAlgorithm(String X, String Y){
+        int[][] A = new int[X.length() + 1][Y.length() + 1];
+        int m = X.length(), n = Y.length();
+        for(int i = 0;i < m;i++)
+            A[i][0] = i;
+        for(int j = 0;j < n;j++)
+            A[0][j] = j;
+        for(int i = 1;i <= m;i++){
+            char c = X.charAt(i - 1);
+            for(int j = 1;j <= n;j++){
+                char t = Y.charAt(j - 1);
+                int cost = 0;
+                if(c != t){
+                    cost += 2;
+                }
+                A[i][j] = Math.min(A[i - 1][j - 1] + cost, Math.min(A[i - 1][j] + 1, A[i][j - 1] + 1));
+            }
+        }
+        return A[m][n];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
