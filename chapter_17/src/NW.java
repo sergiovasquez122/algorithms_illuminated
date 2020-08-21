0995d67c7efc09385d3467aa8c56adca4096b474
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-21 16:44:20 -0700

    added NW testclient

--- chapter_17/src/NW.java
+++ chapter_17/src/NW.java
@@ -1,11 +1,14 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class NW {
     public static int NWAlgorithm(String X, String Y, int gapCost, int mismatchCost){
         int[][] A = new int[X.length() + 1][Y.length() + 1];
         int m = X.length(), n = Y.length();
-        for(int i = 0;i < m;i++)
-            A[i][0] = i;
-        for(int j = 0;j < n;j++)
-            A[0][j] = j;
+        for(int i = 0;i <= m;i++)
+            A[i][0] = i * gapCost;
+        for(int j = 0;j <= n;j++)
+            A[0][j] = j * gapCost;
         for(int i = 1;i <= m;i++){
             char c = X.charAt(i - 1);
             for(int j = 1;j <= n;j++){
@@ -21,6 +24,12 @@ public class NW {
     }
 
     public static void main(String[] args) {
-
+        In in = new In(args[0]);
+        in.readLine(); // ignore the length of the strings
+        int gapCost = in.readInt(), mismatchCost = in.readInt();
+        in.readLine(); // ignore the end of file
+        String X = in.readLine();
+        String Y = in.readLine();
+        StdOut.println(NWAlgorithm(X, Y, gapCost, mismatchCost));
     }
 }
