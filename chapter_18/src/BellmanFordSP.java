440de8c12562f34982e2794e42acd8a1bf6924bd
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-22 16:37:47 -0700

    partial implementation

--- chapter_18/src/BellmanFordSP.java
+++ chapter_18/src/BellmanFordSP.java
@@ -1,2 +1,24 @@
 public class BellmanFordSP {
+    private double [] distTo;
+
+    public BellmanFordSP(EdgeWeightedDigraph G, int s){
+        distTo = new double[G.V()];
+
+        for(int i = 0;i < G.V();i++)
+            distTo[i] = Double.POSITIVE_INFINITY;
+
+        distTo[s] = 0;
+        for(int pass = 0;pass < G.V();pass++) {
+            for (int v = 0; v < G.V(); v++) {
+                for (DirectedEdge e : G.adj(v)) {
+                    int from = e.from(), to = e.to();
+                    if(distTo[to] > distTo[from] + e.weight()){
+                        distTo[to] = distTo[from] + e.weight();
+                    }
+                }
+            }
+        }
+    }
+
+
 }
